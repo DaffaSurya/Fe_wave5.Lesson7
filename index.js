9be98487f7ab337e1966c352a4989e36217abe4c
Daffasurya <daffasuryaarrayan@gmail.com>
2022-11-09 09:27:09 +0700

    JS basic

---
+++ index.js
@@ -0,0 +1,57 @@
+let Father = 4  // 4x umur anaknya 
+let son = 1
+
+let total = 80
+let totalAge = Father+son
+
+let sonAge =  total/totalAge
+
+console.log(sonAge , "Tahun");
+
+
+// menentukan tanggal , bulan dan tahun
+
+// today
+let time1 = new Date();
+console.log(time1.getDate());
+console.log(time1.getMonth());   // dimulai dari 0
+console.log(time1.getFullYear());
+// today
+
+// Yesterday
+
+let time = new Date();          //Date
+time.setDate(8);               
+console.log(time.getDate());
+
+let month = new Date();         //month
+console.log(month.getMonth());
+
+let Years = new Date();          //Years
+console.log(Years.getFullYear());
+
+// yesterday
+
+// tomorrow
+
+let  date = new Date();          //Date
+date.setDate(10);
+console.log(date.getDate());  
+
+let month1 = new Date();         //month
+console.log(month1.getMonth());
+ 
+let Year = new Date();            //Year
+console.log(Year.getFullYear());
+
+// tomorrow
+
+// length string
+
+let Fullname = "Daffa surya arrayan"
+let Shortname = "Daffa"
+
+console.log(Fullname.length);
+console.log(Shortname.length);
+
+// length string Name
